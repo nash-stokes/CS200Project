78412d3225fd0ddd746246d30ea3c6303589bd1f
Manseon <tduongdo@crimson.ua.edu>
2019-11-15 16:58:13 -0600

    Changed Accounting and ProviderTerminal

--- src/main/ProviderTerminal.java
+++ src/main/ProviderTerminal.java
@@ -1,17 +1,24 @@
 package main;
 
+import main.ProviderDirectoryData;
+import main.ServiceController;
+import main.ProviderControler;
+
 public class ProviderTerminal {
   boolean authenticated = false;
-	
+  ServiceController sController;
+  ProviderController pController;
+
   void getProviderDirectory() {
-    
+    if (authenticated)
+      sController.emailPDFFile();
   }
-	
+
   void manageService() {
-    
+
   }
-	
+
   void authenticateProvider() {
-    
+    authenticated = pController.validateProvider();
   }
 }
--- src/main/ServiceController.java
+++ src/main/ServiceController.java
@@ -1,9 +1,8 @@
 public class ServiceController {
-    /* The below attributes must be uncommented when the proper
-     * data classes are implemented
-     * private MemberData memberList[];
-     * private ServiceData serviceList[];
-     * private ProviderData providerList[];
+    /*
+     * The below attributes must be uncommented when the proper data classes are
+     * implemented private MemberData memberList[]; private ServiceData
+     * serviceList[]; private ProviderData providerList[];
      */
     private int countServices;
     private int numProviders;
@@ -11,48 +10,32 @@ public class ServiceController {
     private float individualFee;
 
     public void addService() {
-        /* This function shall:
-         * 1. Be called when the user selects to add a service to the
-         * list of services.
-         * 2. Prompt the user to enter: service date, service time
-         * provider number, member number, service code, and option-
-         * al comments.
-         * 3. Generate a new ServiceData
-         * object with the attributes being filled by their
-         * respective above values.
-         * 4. Prompt the user that
-         * the present data is correct
-         * 5. Store the new object
-         * within serviceList[]
-         * 6. Terminate
+        /*
+         * This function shall: 1. Be called when the user selects to add a service to
+         * the list of services. 2. Prompt the user to enter: service date, service time
+         * provider number, member number, service code, and option- al comments. 3.
+         * Generate a new ServiceData object with the attributes being filled by their
+         * respective above values. 4. Prompt the user that the present data is correct
+         * 5. Store the new object within serviceList[] 6. Terminate
          */
     }
 
     public void emailPDFFile() {
-        /* This function shall:
-         * 1. Be called when the user requests a Provider
-         * Directory
-         * 2. Access ProviderDirectoryInformation and funnel
-         * all necessary data into this function
-         * 3. Aggregate all this function and write it to a text
-         * file
-         * 4. Inform the user that the file has been created
-         * 5. Terminate
+        /*
+         * This function shall: 1. Be called when the user requests a Provider Directory
+         * 2. Access ProviderDirectoryInformation and funnel all necessary data into
+         * this function 3. Aggregate all this function and write it to a text file 4.
+         * Inform the user that the file has been created 5. Terminate
          */
     }
 
     public boolean validateMember() {
-        /* This function shall:
-         * 1. Be called whenever it is necessary that
-         * the existence of a member be validated
-         * 2. Prompt the user to input service code/member name
-         * 3. Iterate through the list of members
-         * 4. Return true if a user is found with matching
-         * credentials
-         * 5. Return falls if no user is found
-         * 6. If true, have user validate the information
-         * 7. Validate the member
-         * 8. Terminate
+        /*
+         * This function shall: 1. Be called whenever it is necessary that the existence
+         * of a member be validated 2. Prompt the user to input service code/member name
+         * 3. Iterate through the list of members 4. Return true if a user is found with
+         * matching credentials 5. Return falls if no user is found 6. If true, have
+         * user validate the information 7. Validate the member 8. Terminate
          */
         return false;
     }
@@ -72,6 +55,6 @@ public class ServiceController {
 
     // UNCOMMENT ONCE DATA CLASS IMPLEMENTED
     // public ServiceData[] getServiceList() {
-    //     return serviceList;
+    // return serviceList;
     // }
 }
