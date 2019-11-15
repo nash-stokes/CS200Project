b7d716a8d787171c2c606e7cffa815f8df4128ea
shjeon99 <shjeon@crimson.ua.edu>
2019-11-15 17:19:23 -0600

    change

--- src/main/AccountsPayableReport.java
+++ src/main/AccountsPayableReport.java
@@ -1,24 +1,27 @@
 package main;
 
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class AccountsPayableReport {
 	private ArrayList<ProviderData> providerList = new ArrayList<ProviderData>();
 	private ArrayList<ServiceData> service = new ArrayList<ServiceData>();
-	private int numOfProviders = service.getNumProviders;
-	private int numOfConsults = service.getCountServices;
-	private float overallFeeTotal = service.getTotalFee();
+	private int numOfProviders = providerList.size();
+	private int numOfConsults;
+	private double overallFeeTotal;
 	private int finalConsults = 0;
-	private float finalFeeTotal = 0;
+	private double finalFeeTotal = 0;
 	
-	public void printAccountsPayableReport() {
+	public void printAccountsPayableReport() throws IOException {
 		FileWriter file = new FileWriter("AccountsPayableReport.txt");
 	    PrintWriter writing = new PrintWriter(file);
 		writing.println("Providers to be paid | Number of Consultations | Total Fee: ");
 		for(int x = 0; x < providerList.size(); x++) {
-			writing.println(providerList.getName() + " | " + numOfConsults + " | " + overallFeeTotal);
+			numOfConsults =  service.getCountServices(x); //figure out later
+			overallFeeTotal = service.getTotalFee(x); //figure out later
+			writing.println(providerList.get(x).getName() + " | " + numOfConsults + " | " + overallFeeTotal);
 			finalConsults = finalConsults + numOfConsults;
 			finalFeeTotal = finalFeeTotal + overallFeeTotal;
 		}
@@ -26,5 +29,6 @@ public class AccountsPayableReport {
 		writing.println("Total number of providers: " + numOfProviders);
 		writing.println("Total number of consultations: " + finalConsults);
 		writing.println("Total Fee: " + finalFeeTotal);
+		writing.close();
 	}
-}
+};
--- src/main/ProviderDirectoryData.java
+++ src/main/ProviderDirectoryData.java
@@ -1,22 +1,44 @@
 package main;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class ProviderDirectoryData {
-	private String[] serviceCodes;
-	private String[] serviceNames;
-	private float[] serviceFees;
-	
+	private static final ArrayList<String> serviceCodes = new ArrayList<String>();
+	private static final ArrayList<String> serviceNames = new ArrayList<String>();
+	private static final ArrayList<Double> serviceFees = new ArrayList<Double>();
+	private String codes;
+	private String names;
+	private double fees;
+
+	public void readFile() throws IOException{
+		File file =  new File("ProviderDirectory.txt");
+		FileReader fr = new FileReader(file);
+		BufferedReader br = new BufferedReader(fr);
+		String line;
+		while((line = br.readLine()) != null){
+			
+		}
+		br.close();
+		fr.close();
+	}	
+
+	public static int getSize() {
+		return serviceCodes.size();
+	}
+
 	public String getServiceCodes(int i) {
-		String codes = serviceCodes[i];
+		codes = serviceCodes.get(i);
 		return codes;
 	}
 	
 	public String getServiceNames(int i) {
-		String names = serviceNames[i];
+		names = serviceNames.get(i);
 		return names;
 	}
 	
-	public float getServiceFees(int i) {
-		float fees = serviceFees[i];
+	public double getServiceFees(int i) {
+		fees = serviceFees.get(i);
 		return fees;
 	}
 }
