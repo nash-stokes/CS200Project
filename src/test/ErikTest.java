89ecbf0f0e4bbc53527734857e69f3a60e302da6
eljones7 <eljones7@crimson.ua.edu>
2019-12-02 18:55:28 -0600

    added tests

--- src/test/ErikTest.java
+++ src/test/ErikTest.java
@@ -2,19 +2,46 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import main.MemberController;
+import main.MemberData;
+
 class ErikTest {
-  
+	ArrayList<MemberData> memberList = new ArrayList<MemberData>();
+	MemberData member = new MemberData();
+	
+	/**
+	 * use 'test' as the name, street, city, state.
+	 * use '00000' as the ZIP
+	 * use '000000000' as the member number
+	 */
   @Test
-  void testAddMemberForSuccess() {
-    
+  void testAddMemberForSuccess() { 
+    MemberController.addMember();
+    memberList = MemberController.getMemberList();
+    member = memberList.get(memberList.size() - 1);
+    assertEquals(member.getName(), "test");
   }
 
+  /**
+	 * use 'test' as the name, street, city, state.
+	 * use '00000' as the ZIP
+	 * use '000000000' as the member number
+	 */
 	@Test
   void testDeleteMemberForSuccess() {
+		MemberController.addMember();
+    memberList = MemberController.getMemberList();
+    int sizeBeforeDelete = memberList.size();
     
+    MemberController.deleteMember();
+    int sizeAfterDelete = memberList.size();
+    
+    assertNotEquals(sizeBeforeDelete, sizeAfterDelete);
   }
 	
 	@Test
