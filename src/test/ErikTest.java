a7e49cb5c3f7a54e53cf1c204cea0f4bf8b4fd08
eljones7 <eljones7@crimson.ua.edu>
2019-12-02 18:16:21 -0600

    added junit test

---
+++ src/test/ErikTest.java
@@ -0,0 +1,25 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ErikTest {
+  
+  @Test
+  void testAddMemberForSuccess() {
+    
+  }
+
+	@Test
+  void testDeleteMemberForSuccess() {
+    
+  }
+	
+	@Test
+  void testDeleteMemberForFailure() {
+		
+  }
+
+}
